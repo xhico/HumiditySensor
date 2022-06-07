d41bbb6582c68cd52f3e4a0f1ff8a2ad1e96084a
xhico <francgf@gmail.com>
2022-06-07 12:16:21 +0100

    Temp C / Temp F w/ pigpio_dht

--- HumiditySensor.py
+++ HumiditySensor.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/python3
 
-# https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/
-# python3 -m pip install Adafruit_DHT yagmail --no-cache-dir
-# nano /home/pi/.local/lib/python3.7/site-packages/Adafruit_DHT/platform_detect.py
-# elif match.group(1) == 'BCM2711':
-#    return 3
-
 import datetime
 import json
-
-import Adafruit_DHT
 import yagmail
+from pigpio_dht import DHT22
+
 
 
 def get911(key):
@@ -20,41 +14,35 @@ def get911(key):
     return data[key]
 
 
+# Email Settings
 EMAIL_USER = get911('EMAIL_USER')
 EMAIL_APPPW = get911('EMAIL_APPPW')
 EMAIL_RECEIVER = get911('EMAIL_RECEIVER')
 
 # Sensor Settings
-DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
+DHT_SENSOR = DHT22(DHT_PIN)
+
 
 if __name__ == "__main__":
     # Get humidity, temperature and date now
-    date_now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    date_now = str(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
 
-    try:
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-        humidity = round(humidity, 1)
-        temperature = round(temperature, 1)
-        print("Humidity: " + str(humidity))
-        print("Temperature: " + str(temperature))
-        
-        # Check if room is on FIRE!!!
-        if temperature > 30:
-            yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "FIRE!! FIRE!! FIRE!!",
-                                                       "Temperature: " + str(temperature) + "°C" "\n" +
-                                                       "Humidity: " + str(humidity) + "%" + "\n" +
-                                                       "Date: " + str(date_now)
-                                                       )
-    except Exception as ex:
-        print(ex)
-        humidity = "None"
-        temperature = "None"
+    # Get Sensor Info
+    temp_c, temp_f, humidity, valid = DHT_SENSOR.read().values()
+    temp_c, temp_f, humidity, valid = str(temp_c), str(temp_f), str(humidity), str(valid)
+    print("temp_c: " + temp_c)
+    print("temp_f: " + temp_f)
+    print("Humidity: " + humidity)
+    print("Valid: " + valid)
+    
+    # Check if room is on FIRE!!!
+    if int(float(temp_c)) > 30:
+        bodyContent = "temp_c: " + temp_c + "°C" + "\n" + "temp_f: " + temp_f + "°F" + "\n" + "Humidity: " + humidity + "%" + "\n" + "Date: " + date_now + "\n" + "Valid: " + valid
+        yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "FIRE!! FIRE!! FIRE!!", bodyContent)
 
     # Save info to file
     with open('/home/pi/HumiditySensor/HumiditySensor.txt', mode='w') as csvFile:
-        csvFile.writelines(["date" + ",humidity" + "," + "temperature", "\n",
-                            str(date_now) + "," + str(humidity) + "," + str(temperature)])
-    csvFile.close()
+        csvFile.writelines(["date, temp_c, temp_f, humidity, valid\n", date_now + ", " + temp_c + ", " + temp_f, ", " + humidity + ", " + valid])
 
 
