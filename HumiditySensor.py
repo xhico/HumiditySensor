ac4564f53f5645211d30f1c9a42ee09f12e6650b
xhico <francgf@gmail.com>
2022-06-09 17:29:59 +0100

    Retry 5 times if failed

--- HumiditySensor.py
+++ HumiditySensor.py
@@ -5,7 +5,7 @@ import datetime
 import json
 import yagmail
 from pigpio_dht import DHT22
-
+import os
 
 
 def get911(key):
@@ -24,30 +24,47 @@ DHT_PIN = 4
 DHT_SENSOR = DHT22(DHT_PIN)
 
 
-if __name__ == "__main__":
+def sendMain(temp_c, temp_f, humidity, date_now):
+    bodyContent = "temp_c: " + temp_c + "°C" + "\n" + "temp_f: " + temp_f + "°F" + "\n" + "Humidity: " + humidity + "%" + "\n" + "Date: " + date_now
+    yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "FIRE!! FIRE!! FIRE!!", bodyContent)
+
+
+def getTemp():
+    try:
+        temp_c, temp_f, humidity, valid = DHT_SENSOR.read().values()
+        temp_c, temp_f, humidity, valid = str(temp_c), str(temp_f), str(humidity), str(valid)
+    except Exception:
+        temp_c, temp_f, humidity, valid = "None", "None", "None", "None"
+
+    return temp_c, temp_f, humidity, valid
+
+
+def main():
     # Get humidity, temperature and date now
     date_now = str(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
 
     # Get Sensor Info
-    try:
-        temp_c, temp_f, humidity, valid = DHT_SENSOR.read().values()
-        temp_c, temp_f, humidity, valid = str(temp_c), str(temp_f), str(humidity), str(valid)
+    temp_c, temp_f, humidity, valid = getTemp()
 
-        # Check if room is on FIRE!!!
-        if int(float(temp_c)) > 30:
-            bodyContent = "temp_c: " + temp_c + "°C" + "\n" + "temp_f: " + temp_f + "°F" + "\n" + "Humidity: " + humidity + "%" + "\n" + "Date: " + date_now + "\n" + "Valid: " + valid
-            yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "FIRE!! FIRE!! FIRE!!", bodyContent)
+    # Retry if failed
+    counter = 1
+    while (valid == "None" or valid == "False") and counter < 5:
+        print(counter, "Retry")
+        temp_c, temp_f, humidity, valid = getTemp()
+        counter += 1
 
-    except Exception:
-        temp_c, temp_f, humidity, valid = "None", "None", "None", "None"
+    print("temp_c: " + temp_c + "\n" + "temp_f: " + temp_f + "\n" + "Humidity: " + humidity + "\n" + "Valid: " + valid)
 
+    # Check if room is on FIRE!!!
+    if int(float(temp_c)) > 30 and temp_c != "None":
+        print("Fire")
+        sendMain(temp_c, temp_f, humidity, date_now)
 
     # Save info to file
-    print("temp_c: " + temp_c)
-    print("temp_f: " + temp_f)
-    print("Humidity: " + humidity)
-    print("Valid: " + valid)
-    with open('/home/pi/HumiditySensor/HumiditySensor.txt', mode='w') as csvFile:
+    LOG_FILE = os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), "HumiditySensor.txt"))
+    with open(LOG_FILE, mode='w') as csvFile:
         csvFile.writelines(["date, temp_c, temp_f, humidity, valid\n", date_now + ", " + temp_c + ", " + temp_f, ", " + humidity + ", " + valid])
 
 
+if __name__ == "__main__":
+    main()
