5aa57ae8c781931a4fff0b6762737562d3a38ebd
xhico <francgf@gmail.com>
2023-10-30 00:05:51 +0000

    Add SenseHat infos

--- HumiditySensor.py
+++ HumiditySensor.py
@@ -12,121 +12,81 @@ from Misc import get911, sendEmail
 from sense_hat import SenseHat
 
 
-def read_dht22_sensor():
+def read_sensor(sensor):
     """
-    Read temperature and humidity values from the DHT22 sensor.
+    Read sensor data.
+
+    Args:
+        sensor (str): The type of sensor to read ("SenseHat" or "DHT22").
 
     Returns:
-        temp_c (float): Temperature in Celsius.
-        temp_f (float): Temperature in Fahrenheit.
-        humidity (float): Relative humidity in percentage.
-        is_valid (str): "True" if the reading is valid, "False" if not.
-
-    If an error occurs while reading the sensor, all values are set to "None."
+        dict: A dictionary containing sensor measurements.
     """
-
     counter = 0
-    while counter < MAX_RETRIES:
-        try:
-            # Read temperature, humidity, and validity from DHT22 sensor
-            DHT_SENSOR = DHT22(SENSOR_DHT22_PIN)
-            temp_c, temp_f, humidity, is_valid = DHT_SENSOR.read().values()
-            is_valid = "False" if temp_c == 0.0 or temp_f == 0.0 or humidity == 0.0 else str(is_valid)
+    info = {"datetime": str(datetime.datetime.now().strftime("%Y/%m/%d %H:%M")), "temp_c": 0, "temp_f": 0, "humidity": 0}
 
-            # If the reading is valid, return the values
-            if is_valid == "True":
-                return temp_c, temp_f, humidity, is_valid
-        except Exception as ex:
-            pass  # Continue to the next retry
-
-        counter += 1
-
-    # If all retries fail, set all values to "None"
-    return "None", "None", "None", "None"
-
-
-def read_senseHat():
-    counter = 0
-    while counter < MAX_RETRIES:
-        try:
-            # Read temperature, humidity from Sense Hat
+    while any(val == 0 for val in info.values()):
+        if sensor == "SenseHat":
             sense = SenseHat()
-            sense.clear()
-            temp_c = sense.get_temperature()
-            temp_f = temp_c * 9 / 5 + 32
-            humidity = sense.get_humidity()
+            info["temp_c"] = round(sense.get_temperature(), 2)
+            info["temp_f"] = round(info["temp_c"] * 9 / 5 + 32, 2)
+            info["humidity"] = round(sense.get_humidity(), 2)
+            info["pressure"] = round(sense.get_pressure(), 2)
 
-            is_valid = "False" if temp_c == 0.0 or temp_f == 0.0 or humidity == 0.0 else "True"
+        if sensor == "DHT22":
+            dht22 = DHT22(SENSOR_DHT22_PIN).read(retries=MAX_RETRIES)
+            info["temp_c"] = round(dht22["temp_c"], 2)
+            info["temp_f"] = round(dht22["temp_f"], 2)
+            info["humidity"] = round(dht22["humidity"], 2)
 
-            # If the reading is valid, return the values
-            if is_valid == "True":
-                return temp_c, temp_f, humidity, is_valid
-        except Exception as ex:
-            print(ex)
-            pass  # Continue to the next retry
+        if counter == 5:
+            return None
 
         counter += 1
 
-    # If all retries fail, set all values to "None"
-    return "None", "None", "None", "None"
+    return info
 
 
 def main():
     """
-    Main function that reads temperature and humidity data from a sensor and saves it to a file.
-
-    If the sensor reading fails, it will retry up to 5 times before giving up.
-    The data is saved to a JSON file with the following format:
-    [
-        {
-            "date": "2023/03/11 10:30",
-            "temp_c": 20.0,
-            "temp_f": 68.0,
-            "humidity": 50.0,
-            "valid": "True"
-        },
-        ...
-    ]
+    Main function to collect sensor data and save it to a file.
     """
-    # Get humidity, temperature and date now
-    date_now = str(datetime.datetime.now().strftime("%Y/%m/%d %H:%M"))
 
-    # Get Sensor Info
-    logger.info("Get Sensor Info")
-    if SENSOR_DHT22_PIN is not False:
-        temp_c, temp_f, humidity, valid = read_dht22_sensor()
-    elif SENSE_HAT is not False:
-        temp_c, temp_f, humidity, valid = read_senseHat()
+    # Set Sensor
+    if SENSE_HAT:
+        sensor = "SenseHat"
+    elif SENSOR_DHT22_PIN:
+        sensor = "DHT22"
     else:
-        logger.error("No valid config")
+        logger.error("Failed to set Sensor, check config")
         return
 
-    # Failed to measure -> exit
-    if valid == "None" or valid == "False":
-        logger.error("Failed to measure -> exit")
+    # Get Measurements
+    logger.info("Sensor: " + sensor)
+    measurements = read_sensor(sensor)
+
+    # Check for valid measurements
+    if measurements is None:
+        logger.error("Failed to get measurements")
         return
 
-    # Log currMeasurements
-    temp_c, temp_f, humidity = round(temp_c, 2), round(temp_f, 2), round(humidity, 2)
-    currMeasurements = {"date": date_now, "temp_c": temp_c, "temp_f": temp_f, "humidity": humidity, "valid": valid}
-    logger.info(currMeasurements)
+    # Log measurements
+    for key, val in measurements.items():
+        logger.info(key + " - " + str(val))
 
-    # Save info to file
+    # Create SAVED_INFO_FILE if it doesn't exist
     if not os.path.exists(SAVED_INFO_FILE):
-        # if the file doesn't exist, create a new empty list
         with open(SAVED_INFO_FILE, "w") as outFile:
             json.dump(list(reversed({})), outFile, indent=2)
 
-    # read the existing data from the file and append the new data to it
+    # Read the existing data from the file and append the new data to it
     with open(SAVED_INFO_FILE) as inFile:
         data = list(reversed(json.load(inFile)))
-        data.append(currMeasurements)
+        data.append(measurements)
 
-    # write the updated data back to the file
+    # Save data
     with open(SAVED_INFO_FILE, "w") as outFile:
         json.dump(list(reversed(data)), outFile, indent=2)
-
-    return
 
 
 if __name__ == "__main__":
@@ -143,8 +103,8 @@ if __name__ == "__main__":
         CONFIG = json.load(inFile)
 
     # Sensor Settings
-    SAVED_INFO_FILE = os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), "saved_info.json"))
     hostname = str(socket.gethostname()).upper()
+    SAVED_INFO_FILE = os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), "saved_info.json"))
     MAX_RETRIES = CONFIG["MAX_RETRIES"]
     SENSOR_DHT22_PIN = CONFIG[hostname]["SENSOR_DHT22_PIN"]
     SENSE_HAT = CONFIG[hostname]["SENSE_HAT"]
